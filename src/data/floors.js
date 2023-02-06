59364e378fdcf943092ca2bedb489c5d6a4c9313
ryan_peterson <ryanjay333@gmail.com>
2023-02-06 09:24:37 -0500

    added floorplates

--- src/data/floors.js
+++ src/data/floors.js
@@ -1,9 +1,14 @@
-import Floorplate1 from '../assets/images/floorplate1.png';
-import Floorplate2 from '../assets/images/floorplate2.png';
 import FloorKeyplan from '../assets/images/keyplan.png';
+import FloorplateLowerLevel from '../assets/floorplates/floorplate-lower-level.svg';
+import FloorplateGround from '../assets/floorplates/floorplate-ground.svg';
+import FloorplateLevelTwo from '../assets/floorplates/floorplate-floor-2.svg';
+import FloorplateLevelThreeFourFive from '../assets/floorplates/floorplate-floor-3-5.svg';
+import FloorplateLevelSixSevenEight from '../assets/floorplates/floorplate-floor-6-8.svg';
 
 export const floors = [
-    { id: 'floor2', label: 'Floor 2', floorplate: Floorplate1, keyplan: FloorKeyplan },
-    { id: 'floor1', label: 'Ground Floor', floorplate: Floorplate2, keyplan: FloorKeyplan },
-    { id: 'ground', label: 'Floor 1', floorplate: Floorplate1, keyplan: FloorKeyplan },
+    { id: 'lowerLevel', label: 'Lower Level', floorplate: FloorplateLowerLevel, keyplan: FloorKeyplan },
+    { id: 'groundFloor', label: 'Ground Floor', floorplate: FloorplateGround, keyplan: FloorKeyplan },
+    { id: 'floorTwo', label: 'Floor Two', floorplate: FloorplateLevelTwo, keyplan: FloorKeyplan },
+    { id: 'floorsThreeFourFive', label: 'Floors 3 - 5', floorplate: FloorplateLevelThreeFourFive, keyplan: FloorKeyplan },
+    { id: 'floorsSixSevenEight', label: 'Floors 6 - 8', floorplate: FloorplateLevelSixSevenEight, keyplan: FloorKeyplan },
 ];
