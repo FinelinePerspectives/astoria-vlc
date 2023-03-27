440625e727e75107267b45d28b2650e91602e38f
Ryan Peterson <rpeterson@finelineperspectives.com>
2023-03-27 15:44:29 -0400

    removed console log from vr tour button

--- src/components/VrTourButton.component.jsx
+++ src/components/VrTourButton.component.jsx
@@ -1,7 +1,6 @@
 import SuitesActionButton from "./SuitesActionButton.component";
 
 const VrTourButton = ({ vrTour, isActive, callback }) => {
-    console.log(vrTour)
     if (vrTour !== undefined && vrTour !== '') {
         return <SuitesActionButton action="virtualtour" isActive={isActive} callback={() => callback(true)} />
     }
